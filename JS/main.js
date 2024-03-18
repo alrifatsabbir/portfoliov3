170a73a2c0309fd055170720e93ccd4230e87edf
alrifatsabbir <alrifat2004@gmail.com>
2024-03-18 21:32:53 +0600

    carousel done

--- JS/main.js
+++ JS/main.js
@@ -32,6 +32,31 @@ window.mouseover= function(event) {
   }
 }
 
+// For Automation of Carousel
+document.addEventListener("DOMContentLoaded", function() {
+  var carousel = document.querySelector("#carouselExampleCaptions");
+  var carouselItems = carousel.querySelectorAll(".carousel-item");
+  var carouselIndicators = carousel.querySelectorAll(".carousel-indicators button");
+  var currentIndex = 0;
+  var interval = 5000; // Set the interval time (in milliseconds)
+
+  function activateIndicator(index) {
+    carouselIndicators.forEach(function(indicator) {
+      indicator.classList.remove("active");
+    });
+    carouselIndicators[index].classList.add("active");
+  }
+
+  function nextSlide() {
+    carouselItems[currentIndex].classList.remove("active");
+    carouselIndicators[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % carouselItems.length;
+    carouselItems[currentIndex].classList.add("active");
+    carouselIndicators[currentIndex].classList.add("active");
+  }
+
+  setInterval(nextSlide, interval);
+});
 
 
 
